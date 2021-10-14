06738ead031d29b5dec012aeffc23048db599324
Tea-son <t_son4@u.pacific.edu>
2021-10-14 15:59:28 -0700

    added audio and moved main camera to become a child of the player

---
+++ Assets/Scripts/BackgroundMusic.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour
+{
+    private static BackgroundMusic music;
+
+    void Awake()
+    {
+        if (music == null)
+        {
+            music = this;
+            DontDestroyOnLoad(music);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
