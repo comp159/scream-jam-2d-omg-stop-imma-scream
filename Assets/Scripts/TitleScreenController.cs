28c0352e4686ea3977d86c8a9ff2d850bf056b58
Prot0ast <a_barragan1@u.pacific.edu>
2021-10-17 17:50:40 -0700

    Added title screen functionality

---
+++ Assets/Scripts/TitleScreenController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreenController : MonoBehaviour
+{
+    [SerializeField] private GameObject playButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playButton.SetActive(true);
+        Time.timeScale = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(sceneName: "GameplayScreen");
+    }
+}
