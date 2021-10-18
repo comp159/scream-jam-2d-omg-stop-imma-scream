312105b642a3dc9913432b148e37e05acf954d0f
Prot0ast <a_barragan1@u.pacific.edu>
2021-10-17 17:30:56 -0700

    It is now possible to lose the game

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,15 +6,30 @@ using Pathfinding;
 
 public class EnemyBehavior : MonoBehaviour
 {
+    private bool playerDead = false;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerDead = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            playerDead = true;
+        }
+    }
+
+    public bool GetPlayerStatus()
+    {
+        return playerDead;
     }
 }
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    private EnemyBehavior enemy;
+    [SerializeField] private GameObject gameOverText;
     // Start is called before the first frame update
     void Start()
     {
+        GameObject e = GameObject.FindGameObjectWithTag("Enemy");
+        enemy = e.GetComponent<EnemyBehavior>();
+        gameOverText.SetActive(false);
         Time.timeScale = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (enemy.GetPlayerStatus() == true)
+        {
+            gameOverText.SetActive(true);
+            Time.timeScale = 0;
+        }
         
     }
 }
