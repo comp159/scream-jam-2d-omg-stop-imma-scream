4ed46727dc7f6c98c507d9eab2c3631007605e39
Prot0ast <a_barragan1@u.pacific.edu>
2021-10-18 22:00:55 -0700

    Making some adjustments to UI and added method to restart game within the game

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
     private EnemyBehavior enemy;
     [SerializeField] private GameObject gameOverText;
+    [SerializeField] private GameObject quitButton;
+    [SerializeField] private GameObject retryButton;
     // Start is called before the first frame update
     void Start()
     {
         GameObject e = GameObject.FindGameObjectWithTag("Enemy");
         enemy = e.GetComponent<EnemyBehavior>();
         gameOverText.SetActive(false);
+        quitButton.SetActive(false);
+        retryButton.SetActive(false);
         Time.timeScale = 1;
     }
 
@@ -22,8 +27,21 @@ public class GameController : MonoBehaviour
         if (enemy.GetPlayerStatus() == true)
         {
             gameOverText.SetActive(true);
+            quitButton.SetActive(true);
+            retryButton.SetActive(true);
             Time.timeScale = 0;
         }
         
+    }
+
+    public void QuitGame()
+    {
+        SceneManager.LoadScene(sceneName: "TitleScreen");
+    }
+
+    public void TryAgain()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
