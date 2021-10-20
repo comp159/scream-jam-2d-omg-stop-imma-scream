305eedfaaabbb4ef2305aa5f5e8dcf13aa5f8d7a
James Nicholas <j_nicholas@u.pacific.edu>
2021-10-20 12:48:47 -0700

    added lvl decoration

---
+++ Assets/Scripts/JummpScareController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JummpScareController : MonoBehaviour
+{
+    [SerializeField] private GameObject jumpScare;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("test");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
