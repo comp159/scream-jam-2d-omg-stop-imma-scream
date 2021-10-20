873252210e24dd87f893f5f20e210ad35cedf16d
Prot0ast <a_barragan1@u.pacific.edu>
2021-10-20 14:15:32 -0700

    Adjusting enemy controller script to have additional behaviors

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -7,17 +7,22 @@ using Pathfinding;
 public class EnemyBehavior : MonoBehaviour
 {
     private bool playerDead = false;
+    private Transform prey;
     
     // Start is called before the first frame update
     void Start()
     {
         playerDead = false;
+        prey = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Vector2.Distance(transform.position, prey.position) > 50)
+        {
+            //play screaming sound here
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
