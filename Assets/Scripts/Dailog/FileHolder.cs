cb6adece861f2f33c801a7208de8899f340fe650
James Nicholas <j_nicholas@u.pacific.edu>
2021-10-22 17:22:58 -0700

    added dialog triggers

---
+++ Assets/Scripts/Dailog/FileHolder.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FileHolder : MonoBehaviour
+{
+    public TextAsset file;
+    
+    public TextAsset getFile()
+    {
+        return file;
+    }
+}
